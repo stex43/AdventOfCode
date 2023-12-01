57ddbe0e80ac89b01c4c996fae7680e1b11d26a6
stex43 <stainy@yandex.ru>
2023-12-01 22:28:37 +0400

    Initial setup

---
+++ AdventOfCode2023/DayTemplate/DayProgram.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode.DayTemplate;
+
+[TestFixture]
+public sealed class Day00 : Solver
+{
+    public Day00() 
+        : base(nameof(Day00).Substring(3, 2))
+    {
+    }
+    
+    [Test]
+    public void Part1()
+    {
+        using var streamReader = this.GetStreamReader();
+
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLine();
+            
+            
+        }
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        using var streamReader = this.GetStreamReader();
+
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLine();
+            
+            
+        }
+    }
+}
---
+++ AdventOfCode2023/Solver.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode;
+
+public abstract class Solver
+{
+    private readonly string dayNumber;
+    
+    protected Solver(string dayNumber)
+    {
+        this.dayNumber = dayNumber;
+    }
+
+    protected StreamReader GetStreamReader()
+    {
+        return new StreamReader($"Day{this.dayNumber}/input.txt");
+    }
+}
