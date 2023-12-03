a7d23af0c90ce4226afe84a11fa77b7d62536ad3
stex43 <stainy@yandex.ru>
2023-12-03 14:53:31 +0400

    Day 3 - version 1.0

---
+++ AdventOfCode2023/Day03/Day03.cs
@@ -0,0 +1,203 @@
+﻿using Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day03;
+
+[TestFixture]
+public sealed class Day03 : Solver
+{
+    public Day03() 
+        : base(nameof(Day03).Substring(3, 2))
+    {
+    }
+    
+    [Test]
+    public void Part1()
+    {
+        using var streamReader = this.GetStreamReader();
+
+        char[,]? schematic = null;
+        var size = 0;
+
+        var lineCount = 0;
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLine();
+
+            if (schematic == null)
+            {
+                size = line!.Length;
+                schematic = new char[size, size];
+            }
+
+            var rowCount = 0;
+            foreach (var symbol in line!)
+            {
+                schematic[lineCount, rowCount] = symbol;
+                rowCount++;
+            }
+
+            lineCount++;
+        }
+
+        var sum = 0;
+
+        for (var i = 0; i < size; i++)
+        {
+            var number = 0;
+            var isPart = false;
+            for (var j = 0; j < size; j++)
+            {
+                var symbol = schematic![i, j];
+                if (!char.IsDigit(symbol))
+                {
+                    if (number != 0 && isPart)
+                    {
+                        sum += number;
+                    }
+
+                    number = 0;
+                    isPart = false;
+                    continue;
+                }
+
+                number = number * 10 + ParseExtensions.ParseDigit(symbol);
+
+                foreach (var neighbourPosition in Arrays.GetNeighbours(i, j, size))
+                {
+                    var neighbour = schematic[neighbourPosition.Item1, neighbourPosition.Item2];
+
+                    if (!char.IsDigit(neighbour) && neighbour != '.')
+                    {
+                        isPart = true;
+                    }
+                }
+            }
+            
+            if (number != 0 && isPart)
+            {
+                sum += number;
+            }
+        }
+        
+        Console.WriteLine(sum);
+        sum.Should().Be(498559);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        using var streamReader = this.GetStreamReader();
+
+        char[,]? schematic = null;
+        var size = 0;
+
+        var lineCount = 0;
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLine();
+
+            if (schematic == null)
+            {
+                size = line!.Length;
+                schematic = new char[size, size];
+            }
+
+            var rowCount = 0;
+            foreach (var symbol in line!)
+            {
+                schematic[lineCount, rowCount] = symbol;
+                rowCount++;
+            }
+
+            lineCount++;
+        }
+
+        var numberList = new List<SchematicNumber>();
+
+        for (var i = 0; i < size; i++)
+        {
+            var number = 0;
+            for (var j = 0; j < size; j++)
+            {
+                var symbol = schematic![i, j];
+                if (!char.IsDigit(symbol))
+                {
+                    if (number != 0)
+                    {
+                        var length = number.ToString().Length;
+                        numberList.Add(new SchematicNumber
+                        {
+                            Number = number,
+                            Line = i,
+                            Row = j - length,
+                            Length = length
+                        });
+                    }
+
+                    number = 0;
+                    continue;
+                }
+
+                number = number * 10 + ParseExtensions.ParseDigit(symbol);
+            }
+
+            numberList.Add(new SchematicNumber
+            {
+                Number = number,
+                Line = i,
+                Row = size - number.ToString().Length,
+                Length = number.ToString().Length
+            });
+        }
+
+        long sum = 0;
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                var symbol = schematic![i, j];
+                if (symbol != '*')
+                {
+                    continue;
+                }
+
+                var listParts = new List<int>();
+                foreach (var schematicNumber in numberList)
+                {
+                    if (Math.Abs(i - schematicNumber.Line) > 1)
+                    {
+                        continue;
+                    }
+
+                    if (Math.Abs(j - schematicNumber.Row) <= 1 ||
+                        Math.Abs(j - schematicNumber.Row - schematicNumber.Length + 1) <= 1 ||
+                        (j >= schematicNumber.Row && j <= schematicNumber.Row + schematicNumber.Length - 1))
+                    {
+                        listParts.Add(schematicNumber.Number);
+                    }
+                }
+
+                if (listParts.Count == 2)
+                {
+                    sum += (long)listParts[0] * (long)listParts[1];
+                }
+            }
+        }
+        
+        Console.WriteLine(sum);
+
+    }
+    
+    private class SchematicNumber
+    {
+        public int Number { get; set; }
+        
+        public int Length { get; set; }
+        
+        public int Row { get; set; }
+        
+        public int Line { get; set; }
+    }
+}
---
+++ Common/Arrays.cs
@@ -0,0 +1,30 @@
+﻿namespace Common;
+
+public static class Arrays
+{
+    public static IEnumerable<Tuple<int, int>> GetNeighbours(int i, int j, int size)
+    {
+        if (i >= size || j >= size || i < 0 || j < 0)
+        {
+            throw new ArgumentOutOfRangeException($"The element ({i}, {j}) is not a part of the array");
+        }
+        
+        var bottomBound = i - 1 >= 0 ? i - 1 : i;
+        var upperBound = i + 1 < size ? i + 1 : i;
+        var leftBound = j - 1 >= 0 ? j - 1 : j;
+        var rightBound = j + 1 < size ? j + 1 : j;
+
+        for (var k = bottomBound; k <= upperBound; k++)
+        {
+            for (var l = leftBound; l <= rightBound; l++)
+            {
+                if (k == i && l == j)
+                {
+                    continue;
+                }
+                
+                yield return Tuple.Create(k, l);
+            }
+        }
+    }
+}
